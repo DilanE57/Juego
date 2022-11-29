06e27800fef2b2627e4cefaf258b15eebe69eb81
Dilan Espinoza <morenoff475@gmail.com>
2022-11-28 20:44:24 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+    
+function user(nombres){
+    if(nombres === "Dilan"){
+        alert("Nucita")
+    }
+    else if(nombres === "Lilian"){
+        alert("Enana Castrosa")
+    }
+    else if(nombres === "Shirley"){
+        alert("Foto carpiño?")
+    }
+    else if(nombres === "Marlyn"){
+        alert("Sebitass Guapo :)")
+    }
+    else if(nombres === "Emerson"){
+        alert("Mañana sin camisa de botones :)")
+    }
+    else{
+        alert("Funcion invalidad refresque la pagina")
+    }
+}
+
+const nombres = prompt("Ingresa alguno de estos nombres, tales como: Dilan , Lilian , Shirley, Marlyn, Emerson")
+
