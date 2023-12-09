04a1982cef186327e8432a9ae2a6237453374ee5
DilanEspinoza <dilan.developer22@gmail.com>
2023-12-08 23:37:37 -0500

    subiendo juego actualizado

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-function user() {
-	const respuesta = prompt(
-		"Ingresa alguno de estos nombres: Dilan , Lilian , Shirley, Marlyn, Emerson"
-	);
+const $botones = document.querySelectorAll("button");
+const apodo = document.getElementById("apodo");
 
-	if (respuesta === "Dilan" || respuesta === "dilan") {
-		alert("Tu apodo sera: nucita");
-	} else if (respuesta === "Lilian" || respuesta === "lilian") {
-		alert(" Tu apodo sera: enana castrosa");
-	} else if (respuesta === "Shirley" || respuesta === "shirley") {
-		alert("foto carpiño?");
-	} else if (respuesta === "Marlyn" || respuesta === "marlyn") {
-		alert("sebitass Guapo :)");
-	} else if (respuesta === "Emerson" || respuesta === "emerson") {
-		alert("mañana sin camisa de botones :)");
-	} else {
-		alert("Este nombre no es valido, preciona el boton nuevamente");
-	}
-}
+$botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    switch (boton.textContent) {
+      case "Dilan":
+        apodo.innerHTML = `El apodo para ${boton.textContent} será: Nucita`;
+        break;
+
+      case "Shirley":
+        apodo.innerHTML = `El apodo para ${boton.textContent} será: Shishi`;
+        break;
+
+      case "Lilian":
+        apodo.innerHTML = `El apodo para ${boton.textContent} será: Mini elfo`;
+        break;
+
+      case "Marlyn":
+        apodo.innerHTML = `El apodo para ${boton.textContent} será: Sebitass guapo :)`;
+        break;
+
+      case "Emerson":
+        apodo.innerHTML = `El apodo para ${boton.textContent} será: Camisa de botones `;
+        break;
+    }
+  });
+});
